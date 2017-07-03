9807749a0df603a8508b24be6f0c2468dac80968
Jerry Tang <thss15_tangrj@163.com>
2017-07-03 16:23:31 +0800

    Update feature_extract.py

--- feature_extract.py
+++ feature_extract.py
@@ -156,10 +156,71 @@ def four_horizental(board, i, j, stone):
 
 	if 5 <= j <= 14 & (board[i][j-5:j+1] == [other,own,own,own,empty,empty] | board[i][j-5:j+1] == [other,own,own,empty,own,empty] | board[i][j-5:j+1] == [other,own,empty,own,own,empty]):
 		return True
+	if 0 <= j <= 9 & (board[i][j:j+6] == [empty,empty,own,own,own,other] | board[i][j:j+6] == [empty,own,empty,own,own,other] | board[i][j:j+6] == [empty,own,own,empty,own,other]):
+		return True
+
+	return False
+
+def four_vertical(board, i, j, stone):
+	own = 0
+	other = 0
+	empty = 0
+	if stone == 'w':
+		own = 1
+		other = 2
+	else:
+		own = 2
+		other = 1
+	if i == 4 & (board[i-4:i+1][j] == [own,own,own,empty,empty] | board[i-4:i+1][j] == [own,own,empty,own,empty] | board[i-4:i+1][j] == [own,empty,own,own,empty]):
+		return True
+	if i == 10 & (board[i:i+5][j] == [empty,empty,own,own,own] | board[i:i+5][j] == [empty,own,empty,own,own] | board[i:i+5][j] == [empty,own,own,empty,own]):
+		return True
+
+	if i == 3 & board[i-3:i+2][j] == [own,own,own,empty,empty]:
+		return True
+	if i == 2 & board[i-2:i+3][j] == [own,own,empty,own,empty]:
+		return True
+	if i == 1 & board[i-1:i+4][j] == [own,empty,own,own,empty]:
+		return True
+	if i == 0 & board[i:i+5][j] == [empty,own,own,own,empty]:
+		return True
+
+	if i == 11 & board[i-1:i+4][j] == [empty,empty,own,own,own]:
+		return True
+	if i == 12 & board[i-2:i+3][j] == [empty,own,empty,own,own]:
+		return True
+	if i == 13 & board[i-3:i+2][j] == [empty,own,own,empty,own]:
+		return True
+	if i == 14 & board[i-4:i+1][j] == [empty,own,own,own,empty]:
+		return True
+
+	if 4 <= i <= 13 & (board[i-4:i+2][j] == [other,own,own,own,empty,empty] | board[i-4:i+2][j] == [empty,own,own,own,empty,other]):
+		return True
+	if 3 <= i <= 12 & (board[i-3:i+3][j] == [other,own,own,empty,own,empty] | board[i-3:i+3][j] == [empty,own,own,empty,own,other]):
+		return True
+	if 2 <= i <= 11 & (board[i-2:i+4][j] == [other,own,empty,own,own,empty] | board[i-2:i+4][j] == [empty,own,empty,own,own,other]):
+		return True
+	if 1 <= i <= 10 & (board[i-1:i+5][j] == [other,empty,own,own,own,empty] | board[i-1:i+5][j] == [empty,empty,own,own,own,other]):
+		return True
+
+	if 5 <= i <= 14 & (board[i-5:i+1][j] == [other,own,own,own,empty,empty] | board[i-5:i+1][j] == [other,own,own,empty,own,empty] | board[i-5:i+1][j] == [other,own,empty,own,own,empty]):
+		return True
+	if 0 <= i <= 9 & (board[i:i+6][j] == [empty,empty,own,own,own,other] | board[i:i+6][j] == [empty,own,empty,own,own,other] | board[i:i+6][j] == [empty,own,own,empty,own,other]):
+		return True
+
+	return False
+
+def four_left_oblique(board, i, j, stone):
+	own = 0
+	other = 0
+	empty = 0
+	if stone == 'w':
+		own = 1
+		other = 2
+	else:
+		own = 2
+		other = 1
 	
-
-
-
 
 # BAAAAB or BAAABA or BAABAA or BABAAA 
 def sealFour(board, i, j, stone):
